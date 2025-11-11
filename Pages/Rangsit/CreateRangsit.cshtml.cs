6a521e425e605450ea5f3a45f3ab37643ba98b2a
AlphsX <tonpylon@gmail.com>
2025-11-12 03:31:41 +0700

    refactor code structure and remove redundant code blocks for improved readability and maintainability

---
+++ Pages/Rangsit/CreateRangsit.cshtml.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace LunaSpaceX.Pages.Rangsit
+{
+    public class CreateRangsitModel : PageModel
+    {
+        public StockInfo stockInfo = new StockInfo();
+        public String errorMessage = "";
+        public String successMessage = "";
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            stockInfo.item = Request.Form["item"];
+            stockInfo.storeid = Request.Form["storeid"];
+            stockInfo.supplier = Request.Form["supplier"];
+            stockInfo.amount = Request.Form["amount"];
+            if (stockInfo.item.Length == 0 || stockInfo.storeid.Length == 0 ||
+                stockInfo.supplier.Length == 0 || stockInfo.amount.Length == 0)
+            {
+                errorMessage = "All the fields're required.";
+                return;
+            }
+            // save the new item into database
+            try
+            {
+                String connectionString = "Server=tcp:lunasx.database.windows.net,1433;Initial Catalog=lunaxservices;Persist Security Info=False;User ID=adscs;Password=Startups01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "INSERT INTO stocks (item, storeid, supplier, amount) VALUES (@item, @storeid, @supplier, @amount);";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@item", stockInfo.item);
+                        command.Parameters.AddWithValue("@storeid", stockInfo.storeid);
+                        command.Parameters.AddWithValue("@supplier", stockInfo.supplier);
+                        command.Parameters.AddWithValue("@amount", stockInfo.amount);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+            }
+            // response
+            stockInfo.item = ""; 
+            stockInfo.storeid = "";
+            stockInfo.supplier = ""; 
+            stockInfo.amount = "";
+            successMessage = "New item added successfully.";
+            Response.Redirect("/Rangsit/IndexRangsit");
+        }
+    }
+}
